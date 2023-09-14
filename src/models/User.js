d3573f17b50f4ad81595e014b73458f1a30bcf66
danteBenitez <benitezdante123@gmail.com>
2023-09-14 11:07:16 -0300

    fix: Claves primarias redundantes en los modelos

--- src/models/User.js
+++ src/models/User.js
@@ -4,45 +4,19 @@ import { Gender } from "./Gender.js";
 import { Location } from "./Location.js";
 import { StudyLevel } from "./StudyLevel.js";
 
-
-export const GENDERS = {
-    MALE: 'masculino',
-    FEMALE: 'femenino',
-}
-
 export const User = sequelize.define('User', {
     age: {
         type: DataTypes.INTEGER(10),
         allowNull: false,
         comment: 'La edad del encuestado'
     },
-    genderId: {
-        type: DataTypes.INTEGER(100),
-        references: {
-            model: Gender,
-        },
-        allowNull: false,
-    },
-    locationId: {
-        type: DataTypes.INTEGER(100),
-        references: {
-            model: Location,
-        },
-        allowNull: false,
-    },
-    studyLevelId: {
-        type: DataTypes.INTEGER(100),
-        references: {
-            model: StudyLevel
-        }
-    }
 });
 
 User.hasOne(Gender);
-Gender.belongsTo(User);
+Gender.hasMany(User);
 
 User.hasOne(Location);
-Location.belongsTo(User);
+Location.hasMany(User);
 
 User.hasOne(StudyLevel);
-StudyLevel.belongsTo(User);
+StudyLevel.hasMany(User);
