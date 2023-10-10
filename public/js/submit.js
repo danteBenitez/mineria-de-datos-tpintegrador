e98cda44a0de981a3caa419fb03c52af571b3248
danteBenitez <benitezdante123@gmail.com>
2023-10-10 16:51:05 -0300

    feat: Añadido manejo de errores

--- public/js/submit.js
+++ public/js/submit.js
@@ -20,22 +20,41 @@ form.addEventListener('submit', async (e) => {
         locationId: formData.get('locationId'),
         studyLevelId: formData.get('studyLevelId')
     };
+    try {
 
-    console.log("User: ", user);
-    console.log("Answers: ", answers);
+        const response = await fetch('/', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                user,
+                answers
+            })
+        });
+        
+        if (!response.ok) throw response;
 
-    const response = await fetch('/', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            user,
-            answers
+    } catch(res) {
+        console.error(res);
+        const { errors } = await res.json();
+        if (!errors) {
+            Swal.fire({
+                icon: 'error',
+                title: 'Error desconocido',
+                footer: 'Contáctese con los desarrolladores del sitio.'
+            });
+            return;
+        }
+        Swal.fire({
+            icon: 'error',
+            title: res.status == 400 ? 'Error de validación' : 'Error desconocido',
+            text: errors[0].msg
         })
-    });
-    const { errors } = await response.json();
-    console.log(errors);
+    }
+
+
+
 });
 
 console.log(surveyQuestions);
